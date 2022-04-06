3eafc22fdb0d9a72bedadb837e432ae8fa4d8f18
Slava-Berasneu <viachaslau.berasneu@uzh.ch>
2022-04-06 23:17:25 +0200

    Added pages, bootstrap dependency. Use -npm install bootstrap

---
+++ src/LoginForm.js
@@ -0,0 +1,20 @@
+<div class='login form' id='loginModal'>
+    <div class='modal content'>
+        <div class='modal body'>
+            <form>
+                <div class="form-group">
+                    <label for="inputUsername">Username</label>
+                    <input type="username" class="form-control" id="inputUsername" placeholder="JohnDoe">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                    </div>
+                <button type="login" class="btn btn-primary">Login</button>
+            </form>
+
+        </div>
+
+    </div>
+
+</div>
