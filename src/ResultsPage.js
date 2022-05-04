42056e675f866bfd34c6754e064d0688257d36f6
Slava-Berasneu <viachaslau.berasneu@uzh.ch>
2022-05-04 14:52:16 +0200

    Squashed commit of the following:
    commit 5836c299d42f076ca77e3422dacc395fe52e713d
    Author: Slava-Berasneu <viachaslau.berasneu@uzh.ch>
    Date:   Wed May 4 14:46:41 2022 +0200
    
        Adjusted parameter names, picture display function
    
    commit 78cce9b41491180c72a0c3ac708b7af1dbce980f
    Merge: 6a42569 dd817ba
    Author: Slava-Berasneu <viachaslau.berasneu@uzh.ch>
    Date:   Wed May 4 12:51:26 2022 +0200
    
        Merge pull request #56 from sopra-fs22-group-15/fix/register-form
    
        removed console.log of request body when sending request
    
    commit dd817ba9057f122b89f85cc926ec987161b27aaa
    Author: daspinkewalross <david@coita.net>
    Date:   Wed May 4 12:00:25 2022 +0200
    
        FIX - removed console.log of request body when sending request
    
    commit 6a425690ebc5b8e540e33069145bfb91c9e93e86
    Merge: d9be2f4 56a3132
    Author: Slava-Berasneu <viachaslau.berasneu@uzh.ch>
    Date:   Wed May 4 00:36:16 2022 +0200
    
        Merge pull request #55 from sopra-fs22-group-15/fix/register-form
    
        fixed register request with correct fields.
    
    commit 56a31321a953cddf9f4d218e046e8667670dbb7d
    Author: daspinkewalross <david@coita.net>
    Date:   Tue May 3 21:55:55 2022 +0200
    
        fixed register request with correct fields.

--- src/ResultsPage.js
+++ src/ResultsPage.js
@@ -1,4 +1,4 @@
-import { Modal, Button, Form, Row, Col, Dropdown, FormLabel, InputGroup, Image, Figure, Container, Badge } from 'react-bootstrap'
+import { Modal, Button, Form, Row, Col, Figure, Container, Badge } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
 import { api, handleError } from './helpers/api'
@@ -9,13 +9,13 @@ function ResultsPage(props) {
     const navigate = useNavigate();
     const [listings, setListings] = useState([])
     let response = null;
-    
+
     const requestResults = async () => {
         try {
             if (!props.query) {
                 // Get the returned listings, create new objects for each.
                 response = await api.get('/listings');
-                
+
             } else {
                 //for future search terms
             }
@@ -31,15 +31,15 @@ function ResultsPage(props) {
                         "name": "flat name",
                         "description": "This is a nice flat for students looking to be close to both the university and the party scene in Zurich",
                         "address": {
-                            "houseNr": 11,
-                            "streetName": "Langstrasse",
+                            "house_number": 11,
+                            "street": "Langstrasse",
                             "city": "Zurich",
-                            "postcode": "8005"
+                            "zip_code": "8005"
                         },
                         "published": true,
                         "pictures": ['https://is1-2.housingcdn.com/4f2250e8/73b4c8375352d2558cc55aeb0bb7f937/v0/fs/devi_shanmuga_flats-surappattu-chennai-devi_flat_promoters.jpeg'],
                         "sqm": 82,
-                        "type": "FLAT",
+                        "listingtype": "FLAT",
                         "furnished": true,
                         "availableTo": ['Male', 'Female', 'Other'],
                         "available": true,
@@ -53,15 +53,15 @@ function ResultsPage(props) {
                         "name": "flat name",
                         "description": "Nice, quiet flat for a low price",
                         "address": {
-                            "houseNr": 20,
-                            "streetName": "Irchelstrasse",
+                            "house_number": 20,
+                            "street": "Irchelstrasse",
                             "city": "Zurich",
-                            "postcode": "8001"
+                            "zip_code": "8001"
                         },
                         "published": true,
-                        "pictures": ['https://bsmedia.business-standard.com/_media/bs/img/article/2018-06/08/full/1528397457-4687.jpg'],
+                        "pictures": null,
                         "sqm": 82,
-                        "type": "FLAT",
+                        "listingtype": "FLAT",
                         "furnished": true,
                         "availableTo": ['Male', 'Female', 'Other'],
                         "available": true,
@@ -75,15 +75,15 @@ function ResultsPage(props) {
                         "name": "flat name",
                         "description": "Located in the heart of the city, perfect for those who like to live a busy life",
                         "address": {
-                            "houseNr": 15,
-                            "streetName": "Paradeplatz",
+                            "house_number": 15,
+                            "street": "Paradeplatz",
                             "city": "Zurich",
-                            "postcode": "8001"
+                            "zip_code": "8001"
                         },
                         "published": true,
                         "pictures": ['https://cf.bstatic.com/xdata/images/hotel/max500/270853248.jpg?k=2a541c2ab4282babde3b0f5d90e00d7cbe4aa382b28876ee59936fd1ed84e5ac&o=&hp=1'],
                         "sqm": 82,
-                        "type": "FLAT",
+                        "listingtype": "HOUSE",
                         "furnished": true,
                         "availableTo": ['Male', 'Female', 'Other'],
                         "available": true,
@@ -97,15 +97,15 @@ function ResultsPage(props) {
                         "name": "flat name",
                         "description": "Small, cheap room close to the university and shops with friendly roommates",
                         "address": {
-                            "houseNr": 11,
-                            "streetName": "Langstrasse",
+                            "house_number": 11,
+                            "street": "Langstrasse",
                             "city": "Zurich",
-                            "postcode": "8005"
+                            "zip_code": "8005"
                         },
                         "published": true,
                         "pictures": ['https://flatfox.ch/media/ff/2022/04/697b6p0hwq2959ebnviuyuedkvnt4yp3qavkhcq2r1w1md12q1.jpg'],
                         "sqm": 82,
-                        "type": "ROOM",
+                        "listingtype": "ROOM",
                         "furnished": true,
                         "availableTo": ['Male', 'Female', 'Other'],
                         "available": true,
@@ -123,14 +123,41 @@ function ResultsPage(props) {
         requestResults();
     }, []);
 
+    const displayPictures = (pictures) => {
+        if (pictures == null) {
+            return (
+                <div>
+                    <Figure>
+                        <Figure.Image
+                            alt="Cannot load image"
+                            src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+                        />
+                    </Figure>
+                </div>);
+        } else {
+            return (<div>
+                <Figure>
+                    <Figure.Image
+                        alt="Cannot load image"
+                        src={pictures[0]}
+                    />
+                </Figure>
+            </div>);
+        }
+    }
+
     const decideColor = (any) => {
         if (any === "FLAT") {
             return (
                 <Badge bg="primary">Flat</Badge>
             )
-        } else {
+        } else if (any === "ROOM") {
             return (
                 <Badge bg="success">Room</Badge>
+            )
+        } else {
+            return (
+                <Badge bg='warning'>House</Badge>
             )
         }
     };
@@ -146,14 +173,7 @@ function ResultsPage(props) {
                     <div className='d-inline-block border border-dark rounded' style={{ marginRight: 20, marginBottom: 3, width: "45%" }} onClick={() => openAdOverview(listing.uuid)}>
                         <Row>
                             <Col md={6}>
-                                <div>
-                                    <Figure>
-                                        <Figure.Image
-                                            alt="Cannot load image"
-                                            src={listing.pictures[0]}
-                                        />
-                                    </Figure>
-                                </div>
+                                {displayPictures(listing.pictures)}
                             </Col>
                             <Col md={6}>
                                 <Row className='g-2'>
@@ -164,34 +184,34 @@ function ResultsPage(props) {
                                         <p>{listing.sqm} m<sup>2</sup></p>
                                     </Col>
                                     <Col md={4}>
-                                        <p>{listing.rooms} Room{listing.rooms>1?'s':null}</p>
+                                        <p>{listing.rooms} Room{listing.rooms > 1 ? 's' : null}</p>
                                     </Col>
                                 </Row>
                                 <Row className='g-2'>
-                                    <h5>{listing.address.streetName} {listing.address.houseNr}, {listing.address.postcode} {listing.address.city}</h5>
+                                    <h5>{listing.address.street} {listing.address.house_number}, {listing.address.zip_code} {listing.address.city}</h5>
                                 </Row>
                                 <Row className='g-2'>
                                     <div>
-                                        {decideColor(listing.type)}
+                                        {decideColor(listing.listingtype)}
                                     </div>
 
                                 </Row>
                                 <Row className='g-2'>
                                     <Col lg='auto'>
-                                    <p>{listing.description}</p>
+                                        <p>{listing.description}</p>
                                     </Col>
                                 </Row>
                                 <Row>
                                     <Col></Col>
                                     <Col>
-                                        
+
                                         <Button type="button" outlined={true} variant="primary">Contact Info</Button>
-                                        
+
                                     </Col>
                                 </Row>
-                                    
-                                
-                               
+
+
+
 
                             </Col>
                         </Row>
